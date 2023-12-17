96163545b8605db64019f6b6d76c94c2b3993416
Gustavo Silva <guh_sz@outlook.com>
2023-12-17 10:35:47 -0300

    aula5

---
+++ Js/index.js
@@ -0,0 +1,7 @@
+const nome = "Luiz";
+const sobre = "Otávio";
+const idade = 32;
+const altura = 1.8;
+const peso = 84;
+const imc = peso /( altura * altura);
+console.log(`Meu nome é ${nome} ${sobre} Tenho ${idade} Anos, minha altura ${altura} é Meu peso é ${peso} e meu imc é de ${imc}`);
