4d01c2eeb6720dca6718f4f9710a95e40dd11631
Gustavo Silva <guh_sz@outlook.com>
2023-12-17 11:24:37 -0300

    aula7

---
+++ Js/aula7/index.js
@@ -0,0 +1,9 @@
+alert("Olá, mundo!")
+window.prompt("Digite seu nome")
+let confirma = confirm ("Tem ctz que quer sair?")
+
+let n1 = prompt("Digite um número:")
+let n2 = prompt("Digite outro número:")
+n1 = Number(n1)
+n2 = Number(n2)
+console.log(`A soma entre esses números é ${n1 + n2}`);
