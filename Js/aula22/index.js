88bfd143fc8b750734ecee8d4021c4baa2fdf919
Gustavo Silva <guh_sz@outlook.com>
2023-12-22 18:00:02 -0300

    aula22

---
+++ Js/aula22/index.js
@@ -0,0 +1,10 @@
+const data = new Date();
+const ano = data.getFullYear();
+console.log(ano)
+
+if (ano<2026) {
+    console.log("Ainda é muito cedo")
+}
+
+const sla = data.getSeconds();
+console.log(sla)
