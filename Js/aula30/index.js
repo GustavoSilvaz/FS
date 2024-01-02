354ae9e8f94b4904467ae678106aa590e9ac6362
Gustavo Silva <guh_sz@outlook.com>
2024-01-02 20:32:02 -0300

    aula 30

---
+++ Js/aula30/index.js
@@ -0,0 +1,14 @@
+const frutas = ['maca', 'pera', 'uva', 'saladamista'];
+
+for (let i in frutas) {
+}
+
+const pessoa = {
+    nome: 'Luix',
+    sobrenome: 'Otavio',
+    idade: 30
+};
+
+for (let chave in pessoa){
+    console.log(chave, pessoa[chave])
+}
