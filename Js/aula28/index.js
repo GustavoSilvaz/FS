6f367b9f6816fd3590795b589d7f8cb069f8d849
Gustavo Silva <guh_sz@outlook.com>
2024-01-02 18:49:47 -0300

    aula28

---
+++ Js/aula28/index.js
@@ -0,0 +1,11 @@
+
+//variável i /condição/ a cada repetição +1
+for (let i = -10; i <=5; i++) {
+    
+}
+
+const frutas = ['pera', 'Uva', 'maca', 'salada-mista' ];
+
+for (let a = 0; a< frutas.length; a++) {
+    console.log(`Eu amo ${frutas[a]}`)
+};
