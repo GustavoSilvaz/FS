85ce94060baac1f59139fd71842b3898078fd030
Gustavo Silva <guh_sz@outlook.com>
2024-01-12 16:20:49 -0300

    aula35

---
+++ Js/aula35/index.js
@@ -0,0 +1,11 @@
+//function ePaisagem (x, y){
+ //   if (x > y) {
+//        return true;
+//    }
+//}
+
+//console.log(ePaisagem(100,50))
+
+const ePaisagem = (largura, altura) => largura > altura;
+
+console.log(ePaisagem(1920, 1080))
