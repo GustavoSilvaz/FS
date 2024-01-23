aa0c51721f258e7f9426056c5b0f0e5fef6914b2
Gustavo Silva <guh_sz@outlook.com>
2024-01-23 12:53:43 -0300

    aula40

---
+++ Js/aula40/index.js
@@ -0,0 +1,20 @@
+function funcao(a, b, c) {
+/*     let total = 0;
+    for(arumento of arguments) {
+        total += arumento;
+    }
+    console.log(total, a, b, c) */
+    console.log(a,b,c)
+}
+
+funcao(1,2,3);
+
+function contas (operador, acumulador, ...numeros) {
+    for (let numero of numeros) {
+        if acumuladora += numero;
+        
+    }
+    console.log(acumuladora);
+}
+
+contas('+', 0, 1, 2);
