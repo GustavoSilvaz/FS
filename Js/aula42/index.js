5ed6aa5eb26cbb70447eb2ef17d57d683f8f5a31
Gustavo Silva <guh_sz@outlook.com>
2024-01-23 13:27:54 -0300

    aula 42

---
+++ Js/aula42/index.js
@@ -0,0 +1,14 @@
+const nome = "Luiz";
+
+function falaNome(){
+    
+    console.log(nome)
+}
+
+falaNome();
+
+function usaFalanome () {
+    const nome ="Otavio";
+    falaNome();
+}
+usaFalanome();
