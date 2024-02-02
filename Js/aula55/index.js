d52dd3bc85b495d2f36a6f98b90575cc94f1d826
Gustavo Silva <guh_sz@outlook.com>
2024-02-02 05:51:58 -0300

    Update index.js

--- Js/aula55/index.js
+++ Js/aula55/index.js
@@ -11,7 +11,13 @@ console.log(pessoa[chave])
 const mano = new Object();
 mano.nome = 'Guzman'
 mano.sobrenome = "Young"
+mano.idade = 30;
+mano.falarNome = function () {
+    return (`${this.nome} está falando seu nome`)
+};
 
 delete mano.sobrenome
 
 console.log(mano)
+
+
