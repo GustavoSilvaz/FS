5b00013f095821038bb9fabb4450eca5d2f02ee5
Gustavo Silva <guh_sz@outlook.com>
2024-02-02 06:34:12 -0300

    aula55

--- Js/aula55/index.js
+++ Js/aula55/index.js
@@ -1,4 +1,4 @@
-const pessoa ={
+/* const pessoa ={
     nome: 'Luiz',
     sobrenome: 'Otavio',
 };
@@ -11,13 +11,53 @@ console.log(pessoa[chave])
 const mano = new Object();
 mano.nome = 'Guzman'
 mano.sobrenome = "Young"
-mano.idade = 30;
+mano.idade = 22;
 mano.falarNome = function () {
-    return (`${this.nome} está falando seu nome`)
+    return (`${this.nome} está falando seu nome`);
 };
+
+mano.falarNome();
 
 delete mano.sobrenome
 
 console.log(mano)
+
+mano.getDataNascimento = function () {
+    const dataAtual = new Date();
+    return dataAtual.getFullYear() - this.idade;
+};
+
+console.log(mano.getDataNascimento());
+
+for (let chave in mano){
+    console.log(mano[chave]);
+}
+ */
+
+/* factory function */
+function criaPessoa(nome, sobrenome) {
+    return {
+        nome,
+        sobrenome,
+        get nomeCompleto(){
+            return `${this.nome} ${this.sobrenome}`;
+        }
+    }
+}
+
+const p1 = criaPessoa("Luiz", "Otavio");
+console.log(p1.nomeCompleto)
+
+/* constructor function */
+function Pessoa(nome, sobrenome) {
+    this.nome = nome;
+    this.sobrenome = sobrenome;
+}
+
+const p2 = new Pessoa("Gustavo", "Silva");
+console.log(p2); 
+
+
+
 
 
