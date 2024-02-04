1109637838e1948c770cf9f484fd1c612ec49a83
Gustavo Silva <guh_sz@outlook.com>
2024-02-04 12:58:21 -0300

    aula59

---
+++ Js/aula59/index.js
@@ -0,0 +1,28 @@
+/* const objetoA = {
+    chaveA: 'A'
+};
+
+const objetoB = {
+    chaveB: 'B'
+};
+
+Object.setPrototypeOf(objetoB, objetoA)
+console.log(objetoB.chaveA) */
+
+function Produto(nome, preco) {
+    this.nome = nome;
+    this.preco = preco;
+
+}
+
+Produto.prototype.desconto = function(percentual) {
+    this.preco = this.preco - (this.preco * (percentual / 100));
+};
+
+Produto.prototype.aumento = function(percentual) {
+    this.preco = this.preco + (this.preco * (percentual / 100));
+};
+
+const p1 = new Produto('Camiseta', 50);
+p1.aumento(100);
+console.log(p1);
