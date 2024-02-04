1a5eb2923d16acdb416f96985f3c93d82c9b806a
Gustavo Silva <guh_sz@outlook.com>
2024-02-04 07:31:36 -0300

    aula57

--- Js/aula57/index.js
+++ Js/aula57/index.js
@@ -2,22 +2,39 @@ function Produto(nome, preco, estoque) {
     this.nome = nome;
     this.preco = preco;
     
-
+    let estoquePrivado = estoque;
     Object.defineProperty(this, "estoque", {
         enumerable: true, //mostra a chave
         //value: estoque, //valor
         //writable: false, //pode alterar?
-        configurable: false, //pode editar ou apagar?
+        configurable: true, //pode editar ou apagar?
         get: function(){
-            return estoque;
+            return estoquePrivado;
         },
         set: function(valor){
-            
+            if(typeof valor !== 'number'){
+                throw new TypeError('Mensagem');
+                }
+            estoquePrivado = valor;
         }
     });
 }
 
-const p1 = new Produto("camiseta", 20, 3);
-console.log(p1);
-p1.estoque = "O valor que eu quero"
-console.log(p1.estoque)
+function criaproduto () {
+    return {
+        get nome(){
+            return nome},
+        set nome(valor)  {
+            valor = valor.replace('coisa', '');
+            nome = valor;
+        },
+    };
+}
+
+const p2 = criaproduto('Camiseta');
+p2.nome = 'qualquer coisa';
+console.log(p2.nome);
+//const p1 = new Produto("camiseta", 20, 3);
+//console.log(p1);
+//p1.estoque = "O valor que eu quero";
+//console.log(p1.estoque)
