7b35b2728ec5a9586a9bff3482c7897f3fecb88e
Gustavo Silva <guh_sz@outlook.com>
2024-02-08 07:31:32 -0300

    aula61

--- Js/aula61/index.js
+++ Js/aula61/index.js
@@ -4,22 +4,34 @@ function Conta(agencia, conta, saldo) {
     this.saldo = saldo;
 }
 
-Conta.prototype.sacar = function(valor){
-    if (this.saldo < valor) {
-        this.verSaldo();
-        return;
-    }
-    this.saldo -= valor;
-    this.verSaldo();
-};
 Conta.prototype.depositar = function(valor){
     this.saldo += valor;
     this.verSaldo();
 };
+
 Conta.prototype.verSaldo = function(){
-    console.log(`Ag/c: ${this.agencia}/${this.conta}` + `Saldo: R$${this.saldo.toFixed(2)}`);
+    console.log(`Ag/c: ${this.agencia}/${this.conta} | ` + `Saldo: R$${this.saldo.toFixed(2)}`);
 };
 
-const conta1 = new Conta(11, 22, 10)
-conta1.depositar(50);
-conta1.sacar(49)
+function CC(agencia, conta, saldo, limite){
+    Conta.call(this, agencia, conta, saldo);
+    this.limite = limite;
+}
+
+CC.prototype = Object.create(Conta.prototype);
+CC.prototype.constructor = CC;
+
+CC.prototype.sacar = function(valor){
+    if (this.saldo + this.limite < valor) {
+        console.log(`Saldo insuficiente R$${this.saldo.toFixed(2)}`)
+        this.verSaldo();
+        return;
+    }
+    
+    this.saldo -= valor;
+    this.verSaldo();
+};
+
+const cc = new  CC(11, 22, 0, 100);
+cc.depositar(10);
+cc.sacar(1117)
