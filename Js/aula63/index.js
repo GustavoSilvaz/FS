e505ac742034e57f0f0fe38435dc07ce942f42c6
Gustavo Silva <guh_sz@outlook.com>
2024-02-08 08:02:23 -0300

    aula63

---
+++ Js/aula63/index.js
@@ -0,0 +1,20 @@
+const pessoas = [
+    {id: 3, nome: 'Luiz'},
+    {id: 2, nome: 'Helena'},
+    {id: 1, nome: 'Maria'}
+
+];
+
+/* const novasPessoas = {};
+for(const pessoa of pessoas) {
+    const {id} = pessoa;
+    novasPessoas[id] = {...pessoa};
+} */
+
+const novasPessoas = new Map();
+for(const pessoa of pessoas) {
+    const {id} = pessoa;
+    novasPessoas.set(id, {...pessoa});
+}
+
+console.log(novasPessoas);
