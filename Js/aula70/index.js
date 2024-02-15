a9fd18a64bfaf6c89bdb1860a8ca34e7ff338525
Gustavo Silva <guh_sz@outlook.com>
2024-02-15 17:17:43 -0300

    aula70

---
+++ Js/aula70/index.js
@@ -0,0 +1,20 @@
+function rand(min, max){
+    min *= 1000;
+    max *= 1000;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+function esperaAi(msg, tempo) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(msg);
+        }, tempo);
+    });
+}
+
+esperaAi('Frase 1', rand(1, 3)).then(resposta => {
+    console.log(resposta)
+    return esperaAi('Frase 2', rand(1, 3))
+}).then(resposta=> {console.log(resposta)}).catch();
+
