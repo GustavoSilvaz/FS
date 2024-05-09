e3dc27c70bac4c3cf35d2147c3cd5f651550713b
Gustavo Silva <guh_sz@outlook.com>
2024-05-08 21:38:48 -0300

    Aula3 6

--- Js/E6 Modules/aula3/src/index.js
+++ Js/E6 Modules/aula3/src/index.js
@@ -1,11 +1,15 @@
 /* Tem como renomear na própria importação
 import { nome as nome2, sobrenome as sla, idade, soma } from './modulo1' */
-import { nome, sobrenome, idade, soma, Pessoa } from './modulo1'
+/* import { nome, sobrenome, idade, soma, Pessoa } from './modulo1' */
 /* importa tudo de uma vez que tiver com export antes da variável
 import * as MeuModulo from './modulo1'; 
 */
 
-const p1 = new Pessoa("Luiz", "Otavio")
+import qualquerNome from './modulo1';
+
+/* const p1 = new Pessoa("Luiz", "Otavio")
 
 console.log(nome, nome, sobrenome, idade , soma(2,3))
-console.log(p1)
+console.log(p1) */
+
+console.log(qualquerNome(2,3))
