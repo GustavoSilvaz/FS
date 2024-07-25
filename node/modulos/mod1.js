d58b68dec1e90a9a8db0cf7966794f66de638447
Gustavo Silva <guh_sz@outlook.com>
2024-07-24 21:57:54 -0300

    esy

---
+++ node/modulos/mod1.js
@@ -0,0 +1,12 @@
+const nome = 'Luiz';
+const sobrenome = "miranda";
+
+const falaNonme = functin () => {
+    console.log(nome, sobrenome)
+};
+
+console.log(module)
+
+module.exports.nome = nome:
+
+console.log(module.exports);
